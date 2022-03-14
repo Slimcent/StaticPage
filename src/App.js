274378284b14092c26b15ccfe48cb9e93ec340ae
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-03-14 13:27:54 +0100

    added the content component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Content from './components/content';
 
 function App() {
   
   return (
     <div>
         <Header/>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <Content/>
         <Footer/>
     </div>
   );
