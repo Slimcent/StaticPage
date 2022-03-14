bec6c490256860ee2761ad663739d884bda5d3e8
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-03-14 13:21:32 +0100

    added Header and Footer components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="../logo192.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
